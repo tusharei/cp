2d702208e1d8cc76b9cf7775092fc17fc565b21a
Tushar Sharma <61770997+tusharei@users.noreply.github.com>
2024-02-08 18:58:12 +0530

    Create 1559A - Mocha and Math.cpp

---
+++ 1559A - Mocha and Math.cpp
@@ -0,0 +1,5 @@
+she can select any [l,r] we dont care
+to minimize the maximum value => take & of whole array 
+
+Intuition =>
+& of whole array is always either equal to minimum element in the array or smaller
